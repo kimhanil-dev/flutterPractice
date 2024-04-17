fb34bf59b4437b2cd4a85451f6f9af7a4f160cae
KimHanIl-dev <khlj752@naver.com>
2024-04-17 17:07:27 +0900

    erase test codes

--- actor_application/acter_project/lib/test_main.dart
+++ actor_application/acter_project/lib/test_main.dart
@@ -1,27 +1,3 @@
-import 'dart:io';
-
-import 'package:acter_project/public.dart';
-
 void main() async {
-  ServerSocket localServer = await ServerSocket.bind('localhost', 55555);
-  Socket toServer = await Socket.connect('localhost', 55555);
-
-  MessageHandler.sendMessage(toServer, MessageType.onAchivement,datas: [15]);
-
-  toServer.listen(
-    (event) {
-      print(String.fromCharCodes(event));
-    },
-  );
-  localServer.listen(
-    (event) {
-      event.listen((event) {
-        var messages = MessageHandler.getMessages(event);
-        for (var message in messages) {
-          print(message.messageType.name);
-          print(message.datas);
-        }
-      });
-    },
-  );
+  
 }
